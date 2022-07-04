c2f98b8bead359710a6a1c6e484d06a747fc7d16
Ryoengineer112358 <ryoengineer112358@gmail.com>
2022-07-04 17:10:45 +0900

    DBからレコードを取得するメソッドの作成

--- work/public/index.php
+++ work/public/index.php
@@ -11,12 +11,27 @@ try {
     DB_PASS,
     [
       PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+      PDO::ATTR_EMULATE_PREPARES => false,
     ]
   );
 } catch (PDOException $e) {
   echo $e->getMessage();
   exit;
 }
+
+function getTodos($pdo)
+{
+  $stmt = $pdo->query("SELECT * FROM todos ORDER BY id DESC");
+  $todos = $stmt->fetchAll();
+  return $todos;
+}
+
+$todos = getTodos($pdo);
+echo ('<pre>');
+var_dump($todos);
+echo ('</pre>');
+exit;
 
 ?>
 <!DOCTYPE html>
